6e25ca79bc0c985aadc21285a603610754fe1c8b
cmanna75 <99143210+cmanna75@users.noreply.github.com>
2023-04-11 16:35:44 -0700

    open/close door

--- debug.c
+++ debug.c
@@ -18,15 +18,12 @@
 #define MYUBRR (FOSC/16/BAUD)-1   // Value for UBRR0 register
 
 
-uint8_t rdata[32];
-uint8_t tdata[32];
+
 void init();
 void serial_out(char);
 void serial_send_string(char*);
 char serial_in();
 void ultrasonic_read();
-void send_wifi();
-void setServoAngle(uint8_t);
 
 int main(){
   init();
@@ -34,6 +31,28 @@ int main(){
   while(1){
     //Toggle Switch
     while (PINC & (1 << PINC1)) { // If PC1 is HIGH
+      
+      /*
+      if( (PINB & (1<<PINB2))){ //if sunlight
+
+        if(PINB & (1<< PINB0)){
+        servo_set(50,180);
+        _delay_ms(50);
+        }
+        else{
+          servo_set(0,180);
+          _delay_ms(50);
+        }   
+
+
+      }   
+      else{ //else night time close door
+        servo_set(0,180);
+        _delay_ms(50);
+      }
+      _delay_ms(50);
+
+      */
       char input = serial_in();
       //ultrasonic
       if(input == 'u'){
@@ -47,18 +66,23 @@ int main(){
       else if(input == 'o'){
         servo_set(50,180);
       }
-      else if(input == 's'){
-        send_wifi();
-      }
       else if(input == 't'){
         serial_send_string("test\n");
       }
       else if(input == '\n'||input == '\r'){
         //if carriage returns do nothing
       }
+      else if(input == 'l'){
+        PORTC |= 1 << PC0;      // Turns on RED debug LED
+        _delay_ms(400);
+        PORTC &= ~(1 << 0);
+        _delay_ms(400);
+        
+      }
       else{
         serial_send_string("invalid command\n");
       }
+      
       _delay_us(10);
     }
     PORTC |= 1 << PC0;      // Turns on RED debug LED
@@ -103,12 +127,15 @@ void init(){
   SWseriale_begin();
 
   //wifi stuff
+  DDRB &= ~(1<<DDB0); //make PB0 an input
 
   
   //Servo stuff
   pwm_init();
   servo_set(0,180);
-
+  
+  //phototransistor
+  DDRB &= ~(1<<DDB2);
   
 }
 
@@ -158,21 +185,9 @@ ISR(PCINT2_vect){
     uint8_t oldSREG = SREG; 
     cli(); //disable interrupt
     char buffer[100];
-    itoa(pulse_time, buffer, 10);
-    serial_send_string(buffer); //send ultrasonic distance
+    itoa(pulse_time, buffer, 10); //ultrasonic sensor distance is 7
+    serial_send_string(buffer); //send ultrasonic distance 
     serial_out('\n');
     SREG = oldSREG; //enable interrupt
   }
-}
-
-
-
-
-void send_wifi(){
-  SWseriale_write("AT",2);
-  while (SWseriale_available()){ // Checks if any character has been received
-	    uint8_t temp = SWseriale_read(); // Reads one character from SWseriale received data buffer
-      char buffer = (char)temp;
-	    serial_out(buffer); // Send one character using SWseriale
-    }
 }
