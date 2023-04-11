6e37b6a45d05f58d70547bf9806d10b9891d7ff6
Will Volpe <will@Wills-MacBook-Pro.local>
2023-04-11 13:27:12 -0700

    esp code

---
+++ ESP32-CAM/birdbox.ino
@@ -0,0 +1,349 @@
+/*
+EE459 Team 18 Project - Bird Box
+Will Volpe, Chris Manna, Aaron Bergen
+
+*/
+
+// Import required libraries
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "index_html.h"
+#include "Arduino.h"
+#include "esp_camera.h"
+
+// Replace with network credentials
+const char* ssid = "Will";
+const char* password = "wifiwill";
+
+bool camState = 0;
+bool ledState = 0;
+bool doorState = 0;
+
+const int camPin = 12;
+const int ledPin = 4;
+const int doorPin = 13;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+// ESP32 AI Thinker Module Pinout
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+typedef struct {
+  camera_fb_t * fb;
+  size_t index;
+} camera_frame_t;
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* STREAM_PART = "Content-Type: %s\r\nContent-Length: %u\r\n\r\n";
+  static const char * JPG_CONTENT_TYPE = "image/jpeg";
+static const char * BMP_CONTENT_TYPE = "image/x-windows-bmp";
+
+class AsyncJpegStreamResponse: public AsyncAbstractResponse {
+  private:
+    camera_frame_t _frame;
+    size_t _index;
+    size_t _jpg_buf_len;
+    uint8_t * _jpg_buf;
+    uint64_t lastAsyncRequest;
+  public:
+  AsyncJpegStreamResponse(){
+    _callback = nullptr;
+    _code = 200;
+    _contentLength = 0;
+    _contentType = STREAM_CONTENT_TYPE;
+    _sendContentLength = false;
+    _chunked = true;
+    _index = 0;
+    _jpg_buf_len = 0;
+    _jpg_buf = NULL;
+    lastAsyncRequest = 0;
+    memset(&_frame, 0, sizeof(camera_frame_t));
+  }
+  ~AsyncJpegStreamResponse(){
+    if(_frame.fb){
+      if(_frame.fb->format != PIXFORMAT_JPEG){
+        free(_jpg_buf);
+      }
+      esp_camera_fb_return(_frame.fb);
+    }
+  }
+  bool _sourceValid() const {
+    return true;
+  }
+  virtual size_t _fillBuffer(uint8_t *buf, size_t maxLen) override {
+    size_t ret = _content(buf, maxLen, _index);
+    if(ret != RESPONSE_TRY_AGAIN){
+        _index += ret;
+    }
+    return ret;
+  }
+  size_t _content(uint8_t *buffer, size_t maxLen, size_t index){
+    if(!_frame.fb || _frame.index == _jpg_buf_len){
+      if(index && _frame.fb){
+        uint64_t end = (uint64_t)micros();
+        int fp = (end - lastAsyncRequest) / 1000;
+        log_printf("Size: %uKB, Time: %ums (%.1ffps)\n", _jpg_buf_len/1024, fp);
+        lastAsyncRequest = end;
+        if(_frame.fb->format != PIXFORMAT_JPEG){
+          free(_jpg_buf);
+        }
+        esp_camera_fb_return(_frame.fb);
+        _frame.fb = NULL;
+        _jpg_buf_len = 0;
+        _jpg_buf = NULL;
+      }
+      if(maxLen < (strlen(STREAM_BOUNDARY) + strlen(STREAM_PART) + strlen(JPG_CONTENT_TYPE) + 8)){
+        //log_w("Not enough space for headers");
+        return RESPONSE_TRY_AGAIN;
+      }
+      //get frame
+      _frame.index = 0;
+
+      _frame.fb = esp_camera_fb_get();
+      if (_frame.fb == NULL) {
+        log_e("Camera frame failed");
+        return 0;
+      }
+
+      if(_frame.fb->format != PIXFORMAT_JPEG){
+        unsigned long st = millis();
+        bool jpeg_converted = frame2jpg(_frame.fb, 80, &_jpg_buf, &_jpg_buf_len);
+        if(!jpeg_converted){
+          log_e("JPEG compression failed");
+          esp_camera_fb_return(_frame.fb);
+          _frame.fb = NULL;
+          _jpg_buf_len = 0;
+          _jpg_buf = NULL;
+          return 0;
+        }
+        log_i("JPEG: %lums, %uB", millis() - st, _jpg_buf_len);
+      } else {
+        _jpg_buf_len = _frame.fb->len;
+        _jpg_buf = _frame.fb->buf;
+      }
+
+      //send boundary
+      size_t blen = 0;
+      if(index){
+        blen = strlen(STREAM_BOUNDARY);
+        memcpy(buffer, STREAM_BOUNDARY, blen);
+        buffer += blen;
+      }
+      //send header
+      size_t hlen = sprintf((char *)buffer, STREAM_PART, JPG_CONTENT_TYPE, _jpg_buf_len);
+      buffer += hlen;
+      //send frame
+      hlen = maxLen - hlen - blen;
+      if(hlen > _jpg_buf_len){
+        maxLen -= hlen - _jpg_buf_len;
+        hlen = _jpg_buf_len;
+      }
+      memcpy(buffer, _jpg_buf, hlen);
+      _frame.index += hlen;
+      return maxLen;
+    }
+  
+    size_t available = _jpg_buf_len - _frame.index;
+    if(maxLen > available){
+      maxLen = available;
+    }
+    memcpy(buffer, _jpg_buf+_frame.index, maxLen);
+    _frame.index += maxLen;
+  
+    return maxLen;
+  }
+};
+
+void streamJpg(AsyncWebServerRequest *request){
+  AsyncJpegStreamResponse *response = new AsyncJpegStreamResponse();
+  if(!response){
+    request->send(501);
+    return;
+  }
+  response->addHeader("Access-Control-Allow-Origin", "*");
+  request->send(response);
+}
+
+// 
+void notifyClients_cam() { ws.textAll(String(camState)); }
+void notifyClients_led() { ws.textAll(String(ledState + 2)); }
+void notifyClients_door() { ws.textAll(String(doorState + 4)); }
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) 
+{
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) 
+  {
+    data[len] = 0;
+    if (strcmp((char*)data, "toggle_cam") == 0) 
+    {
+      camState = !camState;
+      notifyClients_cam();
+    }
+    if (strcmp((char*)data, "toggle_led") == 0) 
+    {
+      ledState = !ledState;
+      notifyClients_led();
+    }
+    if (strcmp((char*)data, "toggle_door") == 0) 
+    {
+      doorState = !doorState;
+      notifyClients_door();
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "CAM_STATE"){
+    if (camState){
+      return "ON";
+    }
+    else{
+      return "OFF";
+    }
+  }
+  if(var == "LED_STATE"){
+    if (ledState){
+      return "ON";
+    }
+    else{
+      return "OFF";
+    }
+  }
+  if(var == "DOOR_STATE"){
+    if (doorState){
+      return "OPEN";
+    }
+    else{
+      return "CLOSED";
+    }
+  }
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; 
+  
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_VGA;
+    config.jpeg_quality = 15;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+  
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  pinMode(camPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(doorPin, OUTPUT);
+
+
+  digitalWrite(camPin, LOW);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(doorPin, LOW);
+
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  initWebSocket();
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/stream", HTTP_GET, streamJpg);
+
+  // Start server
+  server.begin();
+}
+
+void loop() {
+  ws.cleanupClients();
+  digitalWrite(camPin, camState);
+  digitalWrite(ledPin, ledState);
+  digitalWrite(doorPin, doorState);
+
+}
---
+++ ESP32-CAM/index_html.h
@@ -0,0 +1,157 @@
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>Bird Box Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+  html {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+  }
+  img {  
+    width: auto; 
+    max-width: 400px; 
+    height: auto; 
+    padding-top: 20px; 
+  }
+  h1 {
+    font-size: 1.8rem;
+    color: white;
+  }
+  h2{
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #143642;
+  }
+  .topnav {
+    overflow: hidden;
+    background-color: #143642;
+  }
+  body {
+    margin: 0;
+  }
+  .content {
+    padding: 30px;
+    max-width: 600px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+  }
+  .card {
+    background-color: #F8F7F9;;
+    box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);
+    padding-top:10px;
+    padding-bottom:20px;
+    margin-right: 20px;
+  }
+  .button {
+    padding: 15px 50px;
+    font-size: 24px;
+    text-align: center;
+    outline: none;
+    color: #fff;
+    background-color: #0f8b8d;
+    border: none;
+    border-radius: 5px;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+  }
+  .button:hover {
+    background-color: #0f8b8d
+  }
+  .button:active {
+    background-color: #0f8b8d;
+    box-shadow: 2 2px #CDCDCD;
+    transform: translateY(2px);
+  }
+  .state {
+    font-size: 1.5rem;
+    color:#8c8c8c;
+    font-weight: bold;
+  }
+  </style>
+</head>
+<body>
+  <div class="topnav">
+    <h1>Bird Box</h1>
+  </div>
+  <img src="" id="photo">
+  <div class="content">
+    <div class="card">
+      <h2>Camera</h2>
+      <p class="state">state: <span id="cam_state">%CAM_STATE%</span></p>
+      <p><button id="cam_button" class="button">Toggle Camera</button></p>
+    </div>
+    <div class="card">
+      <h2>Flash</h2>
+      <p class="state">state: <span id="led_state">%LED_STATE%</span></p>
+      <p><button id="led_button" class="button">Toggle LED</button></p>
+    </div>
+    <div class="card">
+      <h2>GPIO 13</h2>
+      <p class="state">state: <span id="door_state">%DOOR_STATE%</span></p>
+      <p><button id="door_button" class="button">Toggle Door</button></p>
+    </div>
+  </div>
+<script>
+  var gateway = `ws://${window.location.hostname}/ws`;
+  var websocket;
+  window.addEventListener('load', onLoad);
+  function initWebSocket() {
+    console.log('Trying to open a WebSocket connection...');
+    websocket = new WebSocket(gateway);
+    websocket.onopen    = onOpen;
+    websocket.onclose   = onClose;
+    websocket.onmessage = onMessage; // <-- add this line
+  }
+  function onOpen(event) {
+    console.log('Connection opened');
+  }
+  function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+  }
+
+  function onMessage(event) {
+    switch(event.data)
+    {
+      case '0': document.getElementById("cam_state").innerHTML = "OFF"; document.getElementById('cam_button').style.backgroundColor = "#c90411"; document.getElementById("photo").src = ""; break;
+      case '1': document.getElementById("cam_state").innerHTML = "ON &nbsp;"; document.getElementById('cam_button').style.backgroundColor = "#04b50a"; document.getElementById("photo").src = window.location.href + "stream"; break;
+      case '2': document.getElementById("led_state").innerHTML = "OFF"; document.getElementById('led_button').style.backgroundColor = "#c90411"; break;
+      case '3': document.getElementById("led_state").innerHTML = "ON &nbsp;"; document.getElementById('led_button').style.backgroundColor = "#04b50a"; break;
+      case '4': document.getElementById("door_state").innerHTML = "CLOSED"; document.getElementById('door_button').style.backgroundColor = "#c90411"; break;
+      case '5': document.getElementById("door_state").innerHTML = "OPEN &nbsp;"; document.getElementById('door_button').style.backgroundColor = "#04b50a"; break;
+    }
+  }
+
+  function onLoad(event) {
+    initWebSocket();
+    initButton();
+
+  }
+  function initButton() {
+    document.getElementById('cam_button').addEventListener('click', toggle_cam);
+    document.getElementById('led_button').addEventListener('click', toggle_led);
+    document.getElementById('door_button').addEventListener('click', toggle_door);
+  }
+ 
+  function toggle_cam(){
+    websocket.send('toggle_cam');
+  }
+  function toggle_led(){
+    websocket.send('toggle_led');
+  }
+  function toggle_door(){
+    websocket.send('toggle_door');
+  }
+
+</script>
+</body>
+</html>
+)rawliteral";
